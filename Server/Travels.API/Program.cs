4a2299ccb17cf35d16a86e8d5e03aa8fa350f206
Pafton <ptrojnar3@gmail.com>
2025-04-29 12:54:44 +0200

    Change db

--- Server/Travels.API/Program.cs
+++ Server/Travels.API/Program.cs
@@ -92,9 +92,13 @@ builder.Services.AddCors(options =>
         );
 });
 
-//SQL Server configuration
+//SQL Server configuration ---- MySQL
+/*builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));*/
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        options.UseSqlite("Data Source=./Travels.db")
+        );
 
 
 
--- Server/Travels.Infrastructure/Migrations/20250429105205_ChangedbtoSqlite.Designer.cs
+++ Server/Travels.Infrastructure/Migrations/20250429105205_ChangedbtoSqlite.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Travels.Infrastructure.Presistance;
 
@@ -10,9 +11,11 @@ using Travels.Infrastructure.Presistance;
 namespace Travels.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250429105205_ChangedbtoSqlite")]
+    partial class ChangedbtoSqlite
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
