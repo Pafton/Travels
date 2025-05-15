b4145ebf23a262ec491a2f68546766c37531512b
Pafton <ptrojnar3@gmail.com>
2025-05-15 13:50:57 +0200

    Login component complete

--- Client/Travels.GUI/src/app/login/login.component.ts
+++ Client/Travels.GUI/src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-login',
@@ -24,16 +26,17 @@ export class LoginComponent {
   onSubmit() {
     const loginData = { email: this.email, password: this.password };
 
-    this.http.post<{ token: string }>('http://localhost:5190/api/Account/login', loginData)
-      .subscribe({
-        next: (response) => {
-          this.authService.setToken(response.token);
-          this.router.navigate(['/home']);
-        },
-        error: (err) => {
-          console.error('Błąd logowania:', err);
-          alert('Niepoprawny login lub hasło');
-        }
-      });
+    this.http.post(`${environment.apiUrl}/api/Account/login`, loginData, {
+      responseType: 'text'
+    }).subscribe({
+      next: (token: string) => {
+        this.authService.setToken(token);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        console.error('Błąd logowania:', err);
+        alert('Niepoprawny login lub hasło');
+      }
+    });
   }
 }
